f107a6abd7789ed3f9f2b94f2c5f6609341c547c
Kushwanth-kumar <kushikushipadmanabhuni@gmail.com>
2025-10-16 19:56:06 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserCard from "./UserCard";
+
+function App() {
+  return (
+    <div>
+      <UserCard name="Ramu" age={25} bio="I am a web developer." />
+    </div>
+  );
+}
+
+export default App;
